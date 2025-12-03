936a6009562e4d097992daaf8582ef47313796e3
thanh <th591ngu@htwg-konstanz.de>
2025-12-03 23:59:56 +0100

    env kommentert und main

--- src/main.js
+++ src/main.js
@@ -10,4 +10,4 @@ createApp(App)
   .use(pinia)
   .mount('#app');
 
-console.log('API Base URL:', import.meta.env.VITE_API_BASE_URL)
+//console.log('API Base URL:', import.meta.env.VITE_API_BASE_URL)
